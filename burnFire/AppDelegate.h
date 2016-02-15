4f3a873c96e145783e53e1ba22f45699ab0a9ab8
Hung Nguyen <hungnguyenuit90hp@gmail.com>
2016-02-15 20:00:51 +0700

    Initial Commit

---
+++ burnFire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  burnFire
+//
+//  Created by AragonUit on 2/15/16.
+//  Copyright © 2016 AragonUit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ burnFire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  burnFire
+//
+//  Created by AragonUit on 2/15/16.
+//  Copyright © 2016 AragonUit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
