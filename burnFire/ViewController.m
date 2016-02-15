c6cdfbfe429a736e25321210b1af1c2bc6d027d8
AragonUit <aragonuit@MacBook-Air.local>
2016-02-15 20:46:46 +0700

    first commit

--- burnFire/ViewController.m
+++ burnFire/ViewController.m
@@ -14,11 +14,38 @@
 
 @implementation ViewController
 
+{
+    UIImageView* fire;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.edgesForExtendedLayout =UIRectEdgeNone;
+    [self burnFire];
 }
-
+-(void)burnFire
+{
+    NSMutableArray* images=[[NSMutableArray alloc]initWithCapacity:17];
+    for (int i=1; i<=17; i++) {
+        NSString* fileName;
+        if (i<10) {
+            fileName=[[NSString alloc]initWithFormat:@"campFire0%d.gif",i];
+        }
+        else
+        {
+            fileName=[[NSString alloc]initWithFormat:@"campFire%d.gif",i];
+        }
+        UIImage *image=[UIImage imageNamed:fileName];
+        [images addObject:image];
+    }
+    fire = [[UIImageView alloc]initWithFrame:self.view.bounds];
+    fire.animationImages = images;
+    fire.animationDuration = 1;
+    fire.animationRepeatCount=0;
+    
+    [self.view addSubview:fire];
+    [fire startAnimating];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
